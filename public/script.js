72e9e2a622a59e0584c47d41d92652b1a3bd294b
Nhật Linh <email@gmail.com>
2026-09-10 21:42:45 +0700

    Add TURN server

--- public/script.js
+++ public/script.js
@@ -20,6 +20,16 @@ const config = {
     iceServers: [
         {
             urls: "stun:stun.l.google.com:19302"
+        },
+        {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        },
+        {
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject"
         }
     ]
 };
